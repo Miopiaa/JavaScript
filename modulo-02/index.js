9ef5abbf200bf2c23f82a42b1955ae389784bc22
Miopiaa <rafaelfelipe.martins18@gmail.com>
2022-04-01 15:03:47 -0300

    exercicios feitos ontem

---
+++ modulo-02/index.js
@@ -0,0 +1,10 @@
+for (i = 0;i < 2; i++){
+    console.log("I understand for loops wtice..lol");
+};
+
+var whileUnderstand = 0;
+
+while(whileUnderstand<2){
+    console.log("I understand while loops twice..lol");
+    whileUnderstand++;
+}
